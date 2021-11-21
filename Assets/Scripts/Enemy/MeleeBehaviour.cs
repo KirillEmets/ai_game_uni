50f4ff2c1b4a882c26d130a36a0cc38d49da04a1
KirillEmets <kirill.yemets@gmail.com>
2021-11-21 21:25:12 +0200

    qq

--- Assets/Scripts/Enemy/MeleeBehaviour.cs
+++ Assets/Scripts/Enemy/MeleeBehaviour.cs
@@ -9,8 +9,8 @@ namespace Enemy
             GoToTarget,
             RunAway,
             Attacking
-        }       
-       
+        }  
+        
         public MeleeBehaviour(EnemyController controller) : base(controller)
         {
             
