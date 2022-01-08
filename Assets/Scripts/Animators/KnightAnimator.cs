b98e39be6ac294ebedf49acd46c4103fd4e21ea2
KirillEmets <kirill.yemets@gmail.com>
2022-01-08 11:57:26 +0200

    split animators

---
+++ Assets/Scripts/Animators/KnightAnimator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public abstract class KnightAnimator : MonoBehaviour
+{
+    public Animator animator;
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
+    private static readonly int Attack = Animator.StringToHash("attack");
+    private int _direction = 1;
+
+    public void OnAttackStart()
+    {
+        animator.SetTrigger(Attack);
+    }
+
+    void Update()
+    {
+        animator.SetBool(IsRunning, GetIsRunning());
+        
+        var charDir = GetCharDirection();
+        if (_direction != charDir)
+        {
+            _direction = charDir;
+            var scale = transform.localScale;
+            // ReSharper disable once Unity.InefficientPropertyAccess
+            transform.localScale = new Vector3(charDir, scale.y, scale.z);
+        }
+    }
+
+    public abstract bool GetIsRunning();
+    public abstract int GetCharDirection();
+}
--- Assets/Scripts/Animators/PlayerAnimator.cs
+++ Assets/Scripts/Animators/PlayerAnimator.cs
@@ -3,48 +3,6 @@ using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public abstract class KnightAnimator : MonoBehaviour
-{
-    public Animator animator;
-    private static readonly int IsRunning = Animator.StringToHash("isRunning");
-    private static readonly int Attack = Animator.StringToHash("attack");
-    private int _direction = 1;
-
-    public void OnAttackStart()
-    {
-        animator.SetTrigger(Attack);
-    }
-
-    void Update()
-    {
-        animator.SetBool(IsRunning, GetIsRunning());
-        
-        var charDir = GetCharDirection();
-        if (_direction != charDir)
-        {
-            _direction = charDir;
-            var scale = transform.localScale;
-            // ReSharper disable once Unity.InefficientPropertyAccess
-            transform.localScale = new Vector3(charDir, scale.y, scale.z);
-        }
-    }
-
-    public abstract bool GetIsRunning();
-    public abstract int GetCharDirection();
-}
-
-public class EnemyAnimator : KnightAnimator
-{
-
-    public EnemyController enemyController;
-    public override bool GetIsRunning() => enemyController.GetVelocity().magnitude > 0.1f;
-
-    public override int GetCharDirection()
-    {
-        return  enemyController.GetVelocity().x < 0 ? 1 : -1;
-    }
-}
-
 public class PlayerAnimator : KnightAnimator
 {
     public PlayerController playerController;
