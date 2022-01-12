9ce0661b5fe98a9c4f82b05282928528f3f8bd27
KirillEmets <kirill.yemets@gmail.com>
2022-01-12 15:33:10 +0200

    improve range behaviour

--- Assets/Scripts/Enemy/RangeBehaviour.cs
+++ Assets/Scripts/Enemy/RangeBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RangeBehaviour : EnemyBehaviour
@@ -11,12 +12,13 @@ public class RangeBehaviour : EnemyBehaviour
 
     public override void Update()
     {
-        var pos = (Vector2) Controller.transform.position;
-        var target = (Vector2) Player.transform.position;
+        var myPos = (Vector2) Controller.transform.position;
+        var playerPos = (Vector2) Player.transform.position;
         var moveDirection = Vector2.zero;
-        if (Vector2.Distance(pos, target) <= 6)
+
+        if (Vector2.Distance(myPos, playerPos) <= 6)
         {
-            moveDirection = (pos - target);
+            moveDirection = (myPos - playerPos);
 
             var bestDistance = 0f;
             var bestDirection = Vector2.zero;
@@ -26,12 +28,12 @@ public class RangeBehaviour : EnemyBehaviour
             {
                 const float lookDistance = 5f;
                 var direction = new Vector2(Mathf.Cos(i * Mathf.PI / 6 + rotZ), Mathf.Sin(i * Mathf.PI / 6 + rotZ));
-                var hit = Physics2D.CircleCast(pos, 0.5f, direction, lookDistance, 1 << 9);
+                var hit = Physics2D.CircleCast(myPos, 0.5f, direction, lookDistance, 1 << 9);
 
                 var spareDistance = hit.collider == null ? lookDistance : hit.distance;
-                Debug.DrawRay(pos, direction * spareDistance);
+                Debug.DrawRay(myPos, direction * spareDistance);
 
-                var distanceToPlayer = Vector2.Distance(pos + direction * spareDistance, target);
+                var distanceToPlayer = Vector2.Distance(myPos + direction * spareDistance, playerPos);
                 if (distanceToPlayer > bestDistance)
                 {
                     bestDistance = distanceToPlayer;
@@ -41,6 +43,17 @@ public class RangeBehaviour : EnemyBehaviour
 
             moveDirection = bestDirection;
         }
+
+        var rangeCohorts = Controller.Cohorts.FindAll(c => c != null && c.Weapon == Weapon.Bow);
+        if (rangeCohorts.Count > 0)
+        {
+            var center = rangeCohorts.Aggregate(Vector2.zero,
+                (current, rc) => current + (Vector2) rc.transform.position / rangeCohorts.Count);
+
+            moveDirection += center - myPos;
+        }
+
+        if(moveDirection.magnitude < 0.8f) moveDirection = Vector2.zero;
 
         Controller.SetVelocity(moveDirection.normalized * enemyObject.movementSpeed);
         Controller.StartAttack(new AttackParams(Controller, enemyObject, Player));
